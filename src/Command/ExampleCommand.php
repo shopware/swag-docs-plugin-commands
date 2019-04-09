8f35546cc7a85ffb3f35fe6de351244999008009
Patrick Stahl <p.stahl@shopware.com>
2019-04-09 09:24:05 +0200

    Switch to new structure

--- src/Command/ExampleCommand.php
+++ src/Command/ExampleCommand.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace PluginCommands\Command;
+namespace Swag\PluginCommands\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
---
+++ src/PluginCommands.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Swag\PluginCommands;
+
+use Shopware\Core\Framework\Plugin;
+
+class PluginCommands extends Plugin
+{
+}
